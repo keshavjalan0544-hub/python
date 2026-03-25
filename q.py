e52b54cd17e0769231e43d5d7a5746a8cef8a98f
Keshav Jalan <keshavjalan0544@gmail.com>
2026-03-25 15:52:11 +0530

    Add files via upload

---
+++ q.py
@@ -0,0 +1,5 @@
+'''Wap to input 2 floating pint number and print their average '''
+
+a = float(input("Enter the first no : "))
+b = float(input("Enter the second no : "))
+print("Average : ",(a + b)/2)
